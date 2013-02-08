ba876b945cfaf39bf33dbf48f30ef5e25a481dfa
TLed <tled@ledderboge.de>
2013-02-09 00:35:15 +0100

    fixed version string

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='wxMarkdown',
     description='Displays markdown files as html (using webkit)',
+    version='0.1',
     author='TLed',
     author_email='',
     url='https://github.com/tled/wxMarkdown',
